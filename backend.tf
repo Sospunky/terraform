6e4159a2ccbad2143b5833e1e4be4584793d4314
Sospunky <99449328+Sospunky@users.noreply.github.com>
2022-08-04 17:03:49 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,8 @@
     bucket = "code-sharing-bucket"
     key    = "avinash/s3file-3.tfstate"
     region = "us-west-2"
-    profile = "jespo"
-dynamodb_table= "firstprojecttable"
+    profile = "vickilicious"
+dynamodb_table= "secondprojecttable"
    }
  }
 
